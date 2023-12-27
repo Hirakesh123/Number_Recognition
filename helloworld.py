0ad4e0ddac3439d946f3de76b620b578e84d44b8
Hirakesh123 <42338263+Hirakesh123@users.noreply.github.com>
2023-12-27 15:06:46 +0530

    Add files via upload

---
+++ helloworld.py
@@ -0,0 +1,27 @@
+print('Hello World')
+print('Hello \n World')
+print('''Hello
+World''')
+
+
+print("Hello", end="")
+print("World!")
+
+print("Hello", end=" ")
+print("World!")
+
+name = "Rakesh"
+age = 25
+print("Name: {}, Age: {}".format(name, age))
+
+
+name = "Rakesh"
+age = 25
+print("Name: " + name + ", Age: " + str(age))
+
+name = "Rakesh"
+age = 25
+print(f"Name: {name}, Age: {age}")
+
+
+print("Hello", "World", sep="-", end="!!\n")
